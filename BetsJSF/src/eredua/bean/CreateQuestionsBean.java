08dcb60272a8a38e2c62f16d61fb169fb31691db
Xabier Larrea <reibaxla@gmail.com>
2020-12-13 10:42:07 +0100

    JSF bukatuta

--- BetsJSF/src/eredua/bean/CreateQuestionsBean.java
+++ BetsJSF/src/eredua/bean/CreateQuestionsBean.java
@@ -20,17 +20,37 @@ public class CreateQuestionsBean {
 	private String galdera;
 	private Event event;
 	private float apustua;
-	private List<Event> eventuak = new ArrayList<Event>();
-
+	private List<Event> eventuak = new Vector<Event>();
 	
 	public void onDateSelect(SelectEvent event) {
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Data aukeratua: " + this.data ));
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Data aukeratua: " + this.data ));	
+		BLFacade facadeBL;
+		facadeBL = FacadeBean.getBusinessLogic();
+		eventuak = facadeBL.getEvents(this.data);
+	}
+	
+	public String getGaldera() {
+		return galdera;
+	}
+
+	public void setGaldera(String galdera) {
+		this.galdera = galdera;
+	}
+
+	public float getApustua() {
+		return apustua;
+	}
+
+	public void setApustua(float apustua) {
+		this.apustua = apustua;
 	}
 	
 	public void createQuestions() throws EventFinished, QuestionAlreadyExist {
 		BLFacade facadeBL;
 		facadeBL = FacadeBean.getBusinessLogic();
-		
+		System.out.println(event.getDescription());
+		System.out.println(this.galdera);
+		System.out.println(this.apustua);
 		facadeBL.createQuestion(event, this.galdera, this.apustua);
 	}
 
@@ -43,10 +63,11 @@ public class CreateQuestionsBean {
 	}
 	
 	public List<Event> getEventuak() {
-		BLFacade facadeBL;
-		facadeBL = FacadeBean.getBusinessLogic();
-		eventuak = facadeBL.getEvents(data);
-		return eventuak;
+		return eventuak; 
+	}
+	
+	public void proba() {
+		System.out.println(eventuak);
 	}
 
 	public Event getEvent() {
--- BetsJSF/src/eredua/bean/QueryQuestionsBean.java
+++ BetsJSF/src/eredua/bean/QueryQuestionsBean.java
@@ -2,6 +2,7 @@ package eredua.bean;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Vector;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -9,15 +10,30 @@ import javax.faces.context.FacesContext;
 import org.primefaces.event.SelectEvent;
 
 import businessLogic.BLFacade;
+import domain.Event;
+import domain.Question;
 
 public class QueryQuestionsBean {
-	
 	private Date data;
+	private  List<Question> galderak = new Vector<Question>();
+	private Event event;
+	private List<Event> eventuak = new Vector<Event>();
 
 	public void onDateSelect(SelectEvent event) {
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Data aukeratua: " + event.getObject()));
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Data aukeratua: " + this.event));
+		BLFacade facadeBL;
+		facadeBL = FacadeBean.getBusinessLogic();
+		eventuak = facadeBL.getEvents(this.data);
 	}
 
+	public Event getEvent() {
+		return event;
+	}
+
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+	
 	public Date getData() {
 		return data;
 	}
@@ -25,6 +41,27 @@ public class QueryQuestionsBean {
 	public void setData(Date data) {
 		this.data = data;
 	}
-
 	
+	public void setEvent() {
+		System.out.println(event.getDescription());
+		galderak = event.getQuestions();
+	}
+
+	public List<Event> getEventuak() {
+		return eventuak;
+	}
+
+	public void setEventuak(List<Event> eventuak) {
+		this.eventuak = eventuak;
+	}
+
+
+	public List<Question> getGalderak() {
+		return galderak;
+	}
+
+
+	public void setGalderak(List<Question> galderak) {
+		this.galderak = galderak;
+	}	
 }
